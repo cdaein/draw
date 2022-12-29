3aa979e02ee61f4c0527a2b12c77b02c37c28d87
user <3522003+cdaein@users.noreply.github.com>
2022-12-28 22:42:26 -0500

    first commit

---
+++ README.md
@@ -0,0 +1,17 @@
+# @daeinc/draw
+
+drawing utilities for HTML5 Canvas.
+
+```sh
+npm i @daeinc/draw
+```
+
+then,
+
+```ts
+import { ... } from "@daeinc/draw"
+```
+
+## License
+
+MIT
---
+++ index.test.ts
@@ -0,0 +1,8 @@
+import { describe, expect, test } from "@jest/globals";
+// import { toDomElement } from "./index";
+
+// describe("toDomElement()", () => {
+//   test("test", () => {
+//     // expect(toDomElement('div'));
+//   });
+// });
