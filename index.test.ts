5d5d5a1cbfdeb623b9de3951edcd12bea21fb21b
user <3522003+cdaein@users.noreply.github.com>
2023-01-18 22:55:42 -0500

    wip test

--- index.test.ts
+++ index.test.ts
@@ -1,8 +1,36 @@
 import { describe, expect, test } from "@jest/globals";
-// import { toDomElement } from "./index";
+import "jest-canvas-mock";
+import { toMatchImageSnapshot } from "jest-image-snapshot";
+import { createCanvas } from "@daeinc/canvas";
 
-// describe("toDomElement()", () => {
-//   test("test", () => {
-//     // expect(toDomElement('div'));
-//   });
-// });
+expect.extend({ toMatchImageSnapshot });
+
+// TODO
+describe("test", () => {
+  const {
+    canvas,
+    context: ctx,
+    width,
+    height,
+  } = createCanvas({
+    width: 500,
+    height: 500,
+    pixelRatio: 1,
+    scaleContext: true,
+  });
+
+  ctx.fillStyle = `gray`;
+  ctx.fillRect(0, 0, width, height);
+
+  test("...", () => {
+    // Take a snapshot of the canvas
+    const image = new Image();
+    image.src = canvas.toDataURL();
+
+    // FIX
+    expect(image).toMatchImageSnapshot({
+      customSnapshotsDir: "__image_snapshots__/reference",
+      customSnapshotIdentifier: "reference-image.png",
+    });
+  });
+});
