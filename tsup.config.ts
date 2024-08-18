f263fc039f452d638077621cd20e1f48f1c4e611
MBP14 <3522003+cdaein@users.noreply.github.com>
2024-08-18 14:43:04 -0700

    feat: es6 static build

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,12 +1,36 @@
 import { defineConfig } from "tsup";
 
-export default defineConfig({
-  entry: ["./index.ts"],
-  format: "esm",
-  target: "esnext",
-  dts: true,
-  // splitting: true, // REVIEW
-  // sourcemap: true,
-  clean: true,
-  treeshake: true,
-});
+export default defineConfig([
+  // npm module (no bundling)
+  {
+    entry: ["./index.ts"],
+    format: "esm",
+    target: "esnext",
+    dts: true,
+    clean: true,
+    treeshake: true,
+  },
+  // es6 module (bundled)
+  {
+    entry: {
+      draw: "./index.ts",
+    },
+    format: ["esm"],
+    outDir: "dist",
+    bundle: true,
+    skipNodeModulesBundle: false,
+    target: "esnext",
+    dts: true,
+    splitting: false,
+    sourcemap: true,
+    clean: true,
+    treeshake: true,
+    minify: true,
+    outExtension() {
+      return {
+        js: ".esm.js",
+      };
+    },
+    // noExternal: [/./],
+  },
+]);
